69d43eb06fff43eb503a5da1097015b114c0ea5e
farahmuzaffar <164481851+farahmuzaffar@users.noreply.github.com>
2024-04-15 12:32:55 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+let personName = "John";
+console.log(personName);
+console.log("hello John, would you like to learn javascript?");
+export {};
---
+++ main.ts
@@ -0,0 +1,4 @@
+let personName: string = "John";
+console.log(personName);
+console.log("hello John, would you like to learn javascript?");
+ 
